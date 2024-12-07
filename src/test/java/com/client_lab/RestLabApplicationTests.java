acad8d1d0b24d0d2e98ad902569290c6a1b8ec7a
ufuk058 <ufuksahinduran.uk@gmail.com>
2024-12-07 22:59:34 +0000

    initial commit

---
+++ src/test/java/com/client_lab/RestLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
